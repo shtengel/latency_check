9ef7f80ebe40ce7cc3a9d0b64ab0c818865b931e
Gal Shtengel <gals@skywatch.ai>
2022-03-13 17:51:12 +0200

    added percentile

--- latency_check.js
+++ latency_check.js
@@ -1,16 +1,20 @@
 const { default: axios } = require("axios")
+const percentile = require("percentile");
 
 let counter = 0
 let sum = 0
+let values = []
 
 setInterval(async () => {
     const a = performance.now()
     const data = await axios.get("https://www.mexc.com/open/api/v2/market/depth?symbol=ETH_USDT&depth=100")
     sum += performance.now() - a
     counter = counter + 1
+    values.push(performance.now() - a)
     console.log(`time to fetch orderbook ${performance.now() - a} millis`)
 }, 2000)
 
 setInterval(() => {
     console.log(`average time to fetch (1min): ${sum / counter} millis`)
+    console.log(`${95}% percentile is: ${percentile(95, values)}`)
 }, 1000 * 60)
